b6a9f187cd58352c9d9b509bcb975b706f92325d
Vaibhav <vs630916@gmail.com>
2023-03-02 18:37:47 +0530

    Some Small styling

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
+import { Divider } from "@material-ui/core";
 import Todo from "./Todo";
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
     return (
         <Paper>
             <List>
-                {todos.map(todo => (
-                    <Todo
-                        id={todo.id}
-                        task={todo.task}
-                        key={todo.id}
-                        completed={todo.completed}
-                        removeTodo={removeTodo}
-                        toggleTodo={toggleTodo}
-                        editTodo={editTodo}
-                    />
+                {todos.map((todo, i) => (
+                    <>
+                        <Todo
+                            id={todo.id}
+                            task={todo.task}
+                            key={todo.id}
+                            completed={todo.completed}
+                            removeTodo={removeTodo}
+                            toggleTodo={toggleTodo}
+                            editTodo={editTodo}
+                        />
+                        {i < todo.lenght - 1 && <Divider />}
+                    </>
                 ))}
             </List>
         </Paper>
